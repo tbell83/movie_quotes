6ee47477496f52fd71f366cd2a1c5642c739c447
Thomas Bell <thomasb@aweber.com>
2015-02-25 11:16:36 -0500

    Updating

--- web_server.py
+++ web_server.py
@@ -2,6 +2,7 @@
 import socket
 import argparse
 import sys
+import urllib2
 from random import randint
 from thread import *
 
@@ -11,17 +12,19 @@ parser.add_argument('-s', action='store', dest='input_file')
 
 args = parser.parse_args()
 
+movies = None
+quotes = None
+
 if args.input_file is None:
-    print "Need some quotes!"
-    sys.exit()
+    response = urllib2.urlopen('http://www.coverfire.com/files/quotes.txt')
+    data = response.read()
+    quotes = data.split("\n\n")
+else:
+    input_file = str(args.input_file)
+    fo = open(input_file, 'r')
+    movies = fo.read().split('\n\n\n');
 
 port = int(args.port)
-input_file = str(args.input_file)
-
-print "Loading quotes:"
-fo = open(input_file, 'r')
-movies = fo.read().split('\n\n\n');
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((socket.gethostname(), port))
 server_socket.listen(5)
@@ -29,16 +32,28 @@ print "Listening on port: " + str(port)
 
 
 def get_quote():
-    movie = movies[randint(0,len(movies))].split("\n\n")
-    movie_name = movie[0].split("\n")[0]
-    temp = movie[0].split("\n")
-    temp.pop(0)
-    temp2 = ""
-    for item in temp:
-        temp2 = temp2 + item + "\n" 
-    movie[0] = temp2
-    quote = movie[randint(0,len(movie))]
-    return "<h1>" + movie_name + "</h1>" + "<br/>"  + quote.replace("\n", "<br/>")
+    if movies is not None:
+        movie = movies[randint(0,len(movies))].split("\n\n")
+        credit = movie[0].split("\n")[0]
+        temp = movie[0].split("\n")
+        temp.pop(0)
+        temp2 = ""
+        for item in temp:
+            temp2 = temp2 + item + "\n" 
+        movie[0] = temp2
+        quote = movie[randint(0,len(movie))]
+    elif quotes is not None:
+        quote = quotes[randint(0,len(quotes))]
+        quote = quote.split("\t-- ")
+        if len(quote) > 1:
+            for i in range(1,len(quote)):
+                credit = quote[i].rstrip("\n")
+        quote = quote[0]
+    else:
+        quote = "Something has gone terribly, terribly wrong."
+        credit = "Uh oh..."
+    output = "<h1>" + credit + "</h1>" + "<br/>"  + quote.replace("\n", "<br/>")
+    return output
 
 
 def clientthread(conn):
